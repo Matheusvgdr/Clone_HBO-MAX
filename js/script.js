544481d7abec91d2ba6fd95f568e5cb06144ec8c
matheus victor <matheusvictorgdr15@gmail.com>
2022-10-09 22:43:15 -0300

    Branch aula finalizada

---
+++ js/script.js
@@ -0,0 +1,10 @@
+var redirect = document.querySelector(".espacial");
+var redirect2 = document.querySelector(".terror");
+
+redirect.addEventListener( "click", () => {
+    window.location.href = 'espacial.html';
+});
+
+redirect2.addEventListener( "click", () => {
+    window.location.href = 'terror.html';
+})
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,21 @@ module.exports = {
         'new3': "url('/img/new3.png')",
         'new4': "url('/img/new4.png')",
         'new5': "url('/img/new5.png')",
-        'bg-second-page': "url('/img/starwars.png')"
+        'bg-second-page': "url('/img/starwars.png')",
+        'bg-sobre': "url('/img/bg-3.png')",
+        'bg-espaco': "url('/img/bg-space.png')",
+        'bg-terror': "url('/img/bg-terror.png')",
+        'sp1': "url('/img/sp1.png')",
+        'sp2': "url('/img/sp2.png')",
+        'sp3': "url('/img/sp3.png')",
+        'sp4': "url('/img/sp4.png')",
+        'sp5': "url('/img/sp5.png')",
+        'terr1': "url('/img/terr1.png')",
+        'terr2': "url('/img/terr2.png')",
+        'terr3': "url('/img/terr3.png')",
+        'terr4': "url('/img/terr4.png')",
+        'terr5': "url('/img/terr5.png')"
+
       }
     },
 
@@ -19,8 +33,5 @@ module.exports = {
       'gilroy': ['Gilroy-Bold', 'Gilroy-Medium', 'Gilroy-Regular', 'Gilroy-Light'],
     }
   },
-  plugins: [
-    require('tailwind-scrollbar-hide')
-
-  ],
+  plugins: [ ],
 }
